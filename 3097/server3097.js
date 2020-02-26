dbedbe65ce4a1203c06533f27544a457d53f88a4
Tatsiana Deviaten <tanya.deviaten@gmail.com>
2020-02-26 23:11:33 +0300

    task 3097

--- 3097/server3097.js
+++ 3097/server3097.js
@@ -4,47 +4,47 @@ const webserver = express();
 
 const port = 3097;
 
-webserver.get('/send', (req, res) => {
-    // при обращении по этому УРЛу - просто отдаём строку
-    console.log(`service1 called, req.originalUrl=${req.originalUrl}`);
-    console.log('req', req);
-    res.send();
+const labelStyle = "width:80px;display:inline-block";
+const errorText = "<div style='color: red'>Поле обязательно для заполнентя</div>"
+
+const initForm = [
+    "<form name='login' method=get action='/login' novalidate>",
+    `<span style=${labelStyle}>Login:</span><input type=text name='login' value="">`,
+    `<span style=${labelStyle}>Password:</span><input type=password name='password' value="">`,
+    "<input type='submit' value='Submit'>",
+    "</form>"
+];
+
+webserver.get('/login', (req, res) => {
+
+    let answer = initForm.join('<br/><br/>');
+
+    if (Object.keys(req.query).length) {
+        const {login, password} = req.query;
+
+        if (login && password) {
+            answer = 'Success!'
+        }
+        else {
+            const invalidForm = [...initForm];
+            invalidForm[1] = invalidForm[1].replace(/value="/, `value="${login}`);
+            invalidForm[2] = invalidForm[2].replace(/value=""/, `value="${password}"`);
+
+            if (!login)
+                invalidForm[1]+=errorText;
+
+            if (!password)
+                invalidForm[2]+=errorText;
+
+            answer = invalidForm.join('<br/><br/>')
+        }
+
+
+    }
+
+    res.send(answer)
 });
 
-// webserver.get('/service2', (req, res) => {
-//     // при обращении по этому УРЛу - ответ зависит от GET-параметров
-//     console.log(`service2 called, req.originalUrl=${req.originalUrl}, req.query=`,req.query);
-//     res.send("service2 ok, par1="+req.query.par1+" par2="+req.query.par2);
-// });
-//
-// webserver.get('/service2b/:par1/:par2', (req, res) => {
-//     // при обращении по этому УРЛу - ответ зависит от частей URI запроса
-//     console.log(`service2b called, req.originalUrl=${req.originalUrl}, req.params=`,req.params);
-//     res.send("service2b ok, par1="+req.params.par1+" par2="+req.params.par2);
-// });
-//
-// webserver.get('/service2c', (req, res) => {
-//     // при обращении по этому УРЛу - просто отдаём строку
-//     console.log(`service1 called, req.originalUrl=${req.originalUrl}`);
-//     res.send("service2c ok!");
-// });
-//
-// webserver.get('/service3', (req, res) => {
-//     // при обращении по этому УРЛу - ответ всегда ошибка 401
-//     console.log(`service3 called`);
-//     res.status(401).end();
-// });
-//
-// webserver.get('/service4', (req, res) => {
-//     // при обращении по этому УРЛу - ответ всегда ошибка 401 и в качестве тела ответа - текст ошибки
-//     console.log(`service4 called`);
-//     res.status(401).send("sorry, access denied!");
-// });
-//
-// webserver.get('/service5', (req, res) => {
-//     // при обращении по этому УРЛу - ответа просто не будет
-//     console.log(`service5 called`);
-// });
 
 webserver.listen(port,()=>{ 
     console.log("web server running on port "+port);
